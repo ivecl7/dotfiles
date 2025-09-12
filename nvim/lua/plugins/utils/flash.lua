cb83c91786ddce448c3bd0f1718de4bf1ddc67de
ivecl7 <immortalrover@protonmail.com>
2025-09-12 23:35:01 +0800

    feat: close flash prompt
    It's slow...

--- nvim/lua/plugins/utils/flash.lua
+++ nvim/lua/plugins/utils/flash.lua
@@ -2,7 +2,11 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		prompt = {
+			enabled = false,
+		},
+	},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
