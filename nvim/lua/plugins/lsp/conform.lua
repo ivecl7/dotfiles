c6c272eb2975e129fa9817990cd166c4602e7c95
ivecl7 <immortalrover@protonmail.com>
2025-09-12 15:53:41 +0800

    fix: fix a bug and format the lua code
    rewrite conform.lua

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -1,131 +1,68 @@
 return {
 	"stevearc/conform.nvim",
-	opts = function(_, opts)
-		setup = {
-			format_on_save = function(bufnr)
-				if vim.g.disable_autoformat or vim.b({ bufnr }).disable_autoformat then
-					return
-				end
+	opts = {
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 500,
+		},
 
-				if slow_format_filetypes({ vim.bo({ bufnr }).filetype }) then
-					return
-				end
+		format_after_save = { lsp_fallback = true },
 
-				local function on_format(err)
-					if err and err:match("timeout$") then
-						return slow_format_filetypes({ vim.bo({ bufnr }).filetype })
-					end
-				end
-
-				return { timeout_ms = 200, lsp_fallback = true }
-			end,
-
-			format_after_save = function(bufnr)
-				if vim.g.disable_autoformat or vim.b({ bufnr }).disable_autoformat then
-					return
-				end
-
-				if not slow_format_filetypes({ vim.bo({ bufnr }).filetype }) then
-					return
-				end
-
-				return { lsp_fallback = true }
-			end,
-
-			notify_on_error = true,
-			formatters_by_ft = {
-				html = {
-					"prettierd",
-					"prettier",
-					stop_after_first = true,
-				},
-				css = {
-					"prettierd",
-					"prettier",
-					stop_after_first = true,
-				},
-				cpp = { "clang-format" },
-				javascript = {
-					"prettierd",
-					"prettier",
-					stop_after_first = true,
-				},
-				typescript = {
-					"prettierd",
-					"prettier",
-					stop_after_first = true,
-				},
-				python = {
-					"black",
-					"isort",
-				},
-				lua = { "stylua" },
-				nix = { "nixfmt-rfc-style" },
-				markdown = {
-					"prettierd",
-					"prettier",
-					stop_after_first = true,
-				},
-				yaml = {
-					"prettierd",
-					"prettier",
-					stop_after_first = true,
-				},
-				terraform = { "terraform_fmt" },
-				bicep = { "bicep" },
-				sh = {
-					"shellcheck",
-					"shellharden",
-					"shfmt",
-				},
-				bash = {
-					"shellcheck",
-					"shellharden",
-					"shfmt",
-				},
-				json = { "jq" },
-				rust = { "rustfmt" },
-				_ = { "trim_whitespace" },
+		notify_on_error = true,
+		formatters_by_ft = {
+			html = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
 			},
-
-			formatters = {
-				black = {
-					"black",
-				},
-				["clang-format"] = {
-					"clang-format",
-				},
-				isort = {
-					"isort",
-				},
-				["nixfmt-rfc-style"] = {
-					"nixfmt-rfc-style",
-				},
-				alejandra = {
-					"alejandra",
-				},
-				jq = {
-					"jq",
-				},
-				prettierd = {
-					"prettierd",
-				},
-				stylua = {
-					"stylua",
-				},
-				shellcheck = {
-					"shellcheck",
-				},
-				shfmt = {
-					"shfmt",
-				},
-				shellharden = {
-					"shellharden",
-				},
-				bicep = {
-					"bicep",
-				},
+			css = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
 			},
-		}
-	end,
+			cpp = { "clang-format" },
+			javascript = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			typescript = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			python = {
+				"black",
+				"isort",
+			},
+			lua = { "stylua" },
+			nix = { "nixfmt-rfc-style" },
+			markdown = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			yaml = {
+				"prettierd",
+				"prettier",
+				stop_after_first = true,
+			},
+			terraform = { "terraform_fmt" },
+			bicep = { "bicep" },
+			sh = {
+				"shellcheck",
+				"shellharden",
+				"shfmt",
+			},
+			bash = {
+				"shellcheck",
+				"shellharden",
+				"shfmt",
+			},
+			json = { "jq" },
+			rust = { "rustfmt" },
+			_ = { "trim_whitespace" },
+		},
+	},
 }
