83dbc9ebb7f0fba077a57718c1225401fabb3e71
ivecl7 <immortalrover@protonmail.com>
2025-09-13 22:23:05 +0800

    fix: dismiss some nvim checkhealth warnings
    checkhealth

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,78 +1,81 @@
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
 local o = vim.opt
 
 -- Show line numbers
-o.number = true;
+o.number = true
 -- Show relative line numbers
-o.relativenumber = true;
+o.relativenumber = true
 
 -- Use the system clipboard
-o.clipboard = "unnamedplus";
+o.clipboard = "unnamedplus"
 
 -- Number of spaces that represent a <TAB>
-o.tabstop = 2;
-o.softtabstop = 2;
+o.tabstop = 2
+o.softtabstop = 2
 
 -- Show tabline always
-o.showtabline = 2;
+o.showtabline = 2
 
 -- Use spaces instead of tabs
-o.expandtab = true;
+o.expandtab = true
 
 -- Enable smart indentation
-o.smartindent = true;
+o.smartindent = true
 
 -- Number of spaces to use for each step of (auto)indent
-o.shiftwidth = 2;
+o.shiftwidth = 2
 
 -- Enable break indent
-o.breakindent = true;
+o.breakindent = true
 
 -- Highlight the screen line of the cursor
-o.cursorline = true;
+o.cursorline = true
 
 -- Minimum number of screen lines to keep above and below the cursor
-o.scrolloff = 8;
+o.scrolloff = 8
 
 -- Enable mouse support
-o.mouse = "a";
+o.mouse = "a"
 
 -- Set folding method to manual
-o.foldmethod = "manual";
+o.foldmethod = "manual"
 
 -- Disable folding by default
-o.foldenable = false;
+o.foldenable = false
 
 -- Wrap long lines at a character in 'breakat'
-o.linebreak = true;
+o.linebreak = true
 
 -- Disable spell checking
-o.spell = false;
+o.spell = false
 
 -- Disable swap file creation
-o.swapfile = false;
+o.swapfile = false
 
 -- Time in milliseconds to wait for a mapped sequence to complete
-o.timeoutlen = 300;
+o.timeoutlen = 300
 
 -- Enable 24-bit RGB color in the TUI
-o.termguicolors = true;
+o.termguicolors = true
 
 -- Don't show mode in the command line
-o.showmode = false;
+o.showmode = false
 
 -- Open new split below the current window
-o.splitbelow = true;
+o.splitbelow = true
 
 -- Keep the screen when splitting
-o.splitkeep = "screen";
+o.splitkeep = "screen"
 
 -- Open new split to the right of the current window
-o.splitright = true;
+o.splitright = true
 
 -- Hide command line unless needed
-o.cmdheight = 0;
+o.cmdheight = 0
 
 -- Remove EOB
 o.fillchars = {
-  eob = " ";
-};
+	eob = " ",
+}
