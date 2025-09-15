baccc24a019fec5645de86872a9706844d905e4b
ivecl7 <immortalrover@protonmail.com>
2025-09-15 16:28:32 +0800

    fix: fix a copilot warring message.
    copilot!

--- nvim/lua/plugins/cmp/cmp-copilot.lua
+++ nvim/lua/plugins/cmp/cmp-copilot.lua
@@ -9,9 +9,9 @@ return {
 			copilot_cmp.setup(opts)
 			-- attach cmp source whenever copilot attaches
 			-- fixes lazy-loading issues with the copilot cmp source
-			LazyVim.lsp.on_attach(function()
-				copilot_cmp._on_insert_enter({})
-			end, "copilot")
+			-- LazyVim.lsp.on_attach(function()
+			-- 	copilot_cmp._on_insert_enter({})
+			-- end, "copilot")
 		end,
 	},
 	{
